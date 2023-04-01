9ecb103c25557ece268d2ff301590c57cf102921
Serey Roth <sereyratanakroth@gmail.com>
2023-03-31 18:00:27 -0700

    test(script): create fake user using the new model

--- script.ts
+++ script.ts
@@ -2,12 +2,30 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+    await prisma.user.deleteMany();
+    await prisma.userPreference.deleteMany();
     const user = await prisma.user.create({
         data: {
             firstName: "bob",
-            lastName: "boop"
+            lastName: "bimbo",
+            email: "bob@bob.com",
+            age: 22,
+            userPreference: {
+                create: {
+                    emailUpdates: true
+                }
+            },
+        },
+        select: {
+            firstName: true,
+            lastName: true,
+            userPreference: {
+                select: {
+                    id: true
+                }
+            }
         }
-    });
+    })
 
     console.log(user);
 }
